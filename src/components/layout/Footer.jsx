9260400ca66b1e0738c8f70da8fd3c0140dad319
lipamaoa <filipa.ferreirasilva@gmail.com>
2024-03-27 11:55:27 +0000

    Navbar, footer and app layout started

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="text-sm text-center text-white  bg-slate-950 py-4 md:px-10 px-3">
+      Copyright © 2024
+    </footer>
+  );
+}
+
+export default Footer;
